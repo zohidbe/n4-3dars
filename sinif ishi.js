26049bd9d64ad600942eb789704916331edba55e
zohid <zohid.xayadarov77@gmail.com>
2024-01-08 22:22:24 +0500

    first commit

---
+++ sinif ishi.js
@@ -0,0 +1,63 @@
+// let A = +prompt("ples enter a number ")
+
+// if (Number.isInteger(Number) && number % 2 == 1 ) {
+//     console.log("agar shart tur bosa 1 -blok")
+// } else {
+//     console.log ("agar shart false bosa 2-blok")
+// }
+
+
+
+
+// let A = +prompt("ples enter a namber") ;
+// if (A > 0) {
+//     console.log("A son musbat");
+// } else {
+//     console.log("A son musbat emas yoki 0");
+// }
+
+
+
+// let  = +prompt("soni yozign")
+ 
+// function engKichikSon(a, b, c) {
+  
+//     var minimum = Math.min(a, b, c);
+
+//     return minimum;
+// }
+
+
+// var son1 = 5;
+// var son2 = 2;
+// var son3 = 8;
+
+// var engKichik = engKichikSon(son1, son2, son3);
+// console.log("Eng kichik son: " + engKichik);
+
+
+
+
+
+
+// function generateImage() {
+//     var input = document.getElementById('imageInput');
+//     var canvas = document.getElementById('canvas');
+//     var ctx = canvas.getContext('2d');
+
+//     var file = input.files[0];
+//     var reader = new FileReader();
+
+//     reader.onload = function (e) {
+//         var img = new Image();
+//         img.onload = function () {
+//             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+//         };
+//         img.src = e.target.result;
+//     };
+
+//     reader.readAsDataURL(file);
+// }
+
+
+
---
+++ uyishi.js
@@ -0,0 +1,61 @@
+// 1 - misol
+// var son1 = parseInt(prompt("Birinchi butun sonni kiriting:"));
+// var son2 = parseInt(prompt("Ikkinchi butun sonni kiriting:"));
+
+// if ((son1 % 2 === 1 && son2 % 2 === 0) || (son1 % 2 === 0 && son2 % 2 === 1)) {
+//     alert("Faqat biri toq son");
+// } else {
+//     alert("Ikki sonning hammasi yoki toqlari yoki hammasi juft")
+// }
+
+// // 2 - misol
+
+// var kompyuterNomi = prompt("Iltimos, kompyuter nomini kiriting:");
+
+// var kompyuterSumma = 0;
+// for (var i = 0; i < kompyuterNomi.length; i++) {
+//     kompyuterSumma += kompyuterNomi.charCodeAt(i);
+// }
+
+// if (kompyuterSumma % 2 === 0) {
+//     alert("Kompyuter summasi juft son: " + kompyuterSumma);
+// } else {
+//     alert("Kompyuter summasi toq son: " + kompyuterSumma);
+// }
+// 3 - misol
+
+// var a = parseInt(prompt("Birinchi butun sonni kiriting:"));
+// var b = parseInt(prompt("Ikkinchi butun sonni kiriting:"));
+
+
+// if (a > 0 && b > 0) {
+//     alert("Jumla rostlikka bajariladi");
+// } else {
+//     alert("Jumla rostlikka bajarilmadi");
+// }
+
+
+// 4 - misol
+// var a = parseInt(prompt("a ni kiriting:"));
+// var b = parseInt(prompt("b ni kiriting:"));
+// var c = parseInt(prompt("c ni kiriting:"));
+// var d = parseInt(prompt("d ni kiriting:"));
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     alert("Hamma sonlar musbat");
+// } else {
+//     alert("Barcha sonlar musbat emas");
+// }
+
+// 5 -misol 
+// var son1 = parseInt(prompt("Birinchi butun sonni kiriting:"));
+// var son2 = parseInt(prompt("Ikkinchi butun sonni kiriting:"));
+
+
+// if ((son1 % 2 === 1 && son2 % 2 === 0) || (son1 % 2 === 0 && son2 % 2 === 1)) {
+//     alert("Faqat biri toq son");
+// } else {
+//     alert("Ikki sonning hammasi yoki toqlari yoki hammasi juft");
+// }
+
+// shunchani qiloldim ustoz :))
